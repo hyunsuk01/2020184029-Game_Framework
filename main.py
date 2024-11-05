612706a2216c4e24243307d2195549890f189313
hyunsuk01 <76142195+hyunsuk01@users.noreply.github.com>
2024-11-05 13:15:47 +0900

    완료

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import game_framework
 # 게임 기본 구조
 from pico2d import open_canvas, delay, close_canvas
 # title mode를 import 하되, 이름을 바꿔친다.
-import play_mode as start_mode
+import logo_mode as start_mode
 
 open_canvas()
 game_framework.run(start_mode)
